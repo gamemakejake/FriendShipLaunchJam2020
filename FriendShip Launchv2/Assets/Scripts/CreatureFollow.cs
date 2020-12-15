8dfd8c43ab2c989b3619b5f90a8cdea46c8a1002
Alexander Bonesteele <ajbonesteele@gmail.com>
2020-12-15 10:54:30 -0800

    creature follow's player

---
+++ FriendShip Launchv2/Assets/Scripts/CreatureFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CreatureFollow : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    Transform player;
+    public bool canFollow = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canFollow)
+        {
+            FindPlayer();
+            FollowPlayer();
+        }
+    }
+
+    void FindPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void FollowPlayer()
+    {
+        if (Vector3.Distance(transform.position, player.position) > agent.stoppingDistance)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
