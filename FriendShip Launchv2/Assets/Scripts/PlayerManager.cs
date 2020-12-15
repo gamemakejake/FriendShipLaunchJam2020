f9e7853c1267003cebfdc9771ca7bffff3dfd727
Alexander Bonesteele <ajbonesteele@gmail.com>
2020-12-15 11:02:45 -0800

    changing PlayerManager script to work with Vector2

--- FriendShip Launchv2/Assets/Scripts/PlayerManager.cs
+++ FriendShip Launchv2/Assets/Scripts/PlayerManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     public float speed = 1;
-    Vector3 movement;
+    Vector2 movement;
 
     //public Animator animator;
 
@@ -19,7 +19,7 @@ public class PlayerManager : MonoBehaviour
     void Update()
     {
         //Player flew off the screen with transform.position.y in the line below.  Made it 0 again.
-        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         transform.Translate(movement * speed * Time.deltaTime);
         //Jake did this, but we should change it to L/R Triggers instead
         //animator.SetFloat("SpeedHor", Mathf.Abs(movement.x));
