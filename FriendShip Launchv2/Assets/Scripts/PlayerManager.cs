5ddd06bbf86612f54123d828a16b78244b44e1c5
Alexander Bonesteele <ajbonesteele@gmail.com>
2020-12-14 17:37:31 -0800

    movement for character sprite

---
+++ FriendShip Launchv2/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float speed = 1;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
