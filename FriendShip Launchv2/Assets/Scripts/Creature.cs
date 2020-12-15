5610d788e5061b4e5cf170a06620d06a6a6a95bf
Alexander Bonesteele <ajbonesteele@gmail.com>
2020-12-15 13:49:09 -0800

    still working on creature patrol

---
+++ FriendShip Launchv2/Assets/Scripts/Creature.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Creature : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject[] waypoints;
+    [HideInInspector]
+    public bool atWaypoint = false;
+    [HideInInspector]
+    public int idx = 0;
+
+    void Update()
+    {
+        if (idx >= waypoints.Length)
+        {
+            idx = 0;
+        }
+    }
+}
