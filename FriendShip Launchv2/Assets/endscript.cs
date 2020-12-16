d8a32e6d930dd504b2e375497ad13b494154b776
GameMakeJake <jacobshillan@gmail.com>
2020-12-16 09:43:20 -0800

    end scene

---
+++ FriendShip Launchv2/Assets/endscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endscript : MonoBehaviour
+{
+    public GameObject trig;
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            trig.SetActive(true);
+        }
+
+    }
+
+}
+
+
